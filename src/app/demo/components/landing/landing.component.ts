6faa266fb90e55445854ab6a6b9312f8ed5b1791
Camvan955 <nguyenvann955@gmail.com>
2023-07-13 18:30:29 +0700

    [SBFA-1] create UI landing page

--- src/app/demo/components/landing/landing.component.ts
+++ src/app/demo/components/landing/landing.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
-    templateUrl: './landing.component.html'
+    templateUrl: './landing.component.html',
 })
 export class LandingComponent {
-
     constructor(private layoutService: LayoutService) {}
 
     scrollTo(viewChild: HTMLElement) {
@@ -14,9 +13,9 @@ export class LandingComponent {
 
     get backgroundStyle(): object {
         let path = 'assets/demo/images/landing/';
-        let image = this.layoutService.config.colorScheme === 'dark' ? 'line-effect-dark.svg' : 'line-effect.svg'; 
+        let image = this.layoutService.config.colorScheme === 'dark' ? 'line-effect-dark.svg' : 'line-effect.svg';
 
-        return {'background-image': 'url(' + path + image + ')'};
+        return { 'background-image': 'url(' + path + image + ')' };
     }
 
     get colorScheme(): string {
