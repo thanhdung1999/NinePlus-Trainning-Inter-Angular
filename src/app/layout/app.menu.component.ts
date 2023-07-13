4d3f8b3f6978aabd613641720eb0dd09f688455f
Camvan955 <nguyenvann955@gmail.com>
2023-07-13 18:30:29 +0700

    [SBFA-1] create UI landing page

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -98,117 +98,117 @@ export class AppMenuComponent implements OnInit {
             // },
 
             // TODO
-            // {
-            //     label: 'Pages',
-            //     icon: 'pi pi-fw pi-briefcase',
-            //     items: [
-            //         {
-            //             label: 'Landing',
-            //             icon: 'pi pi-fw pi-globe',
-            //             routerLink: ['/landing'],
-            //             data: { fullPage: true },
-            //         },
-            //         {
-            //             label: 'Auth',
-            //             icon: 'pi pi-fw pi-user',
-            //             items: [
-            //                 {
-            //                     label: 'Login',
-            //                     icon: 'pi pi-fw pi-sign-in',
-            //                     routerLink: ['/auth/login'],
-            //                     data: { fullPage: true },
-            //                 },
-            //                 {
-            //                     label: 'Error',
-            //                     icon: 'pi pi-fw pi-times-circle',
-            //                     routerLink: ['/auth/error'],
-            //                     data: { fullPage: true },
-            //                 },
-            //                 {
-            //                     label: 'Access Denied',
-            //                     icon: 'pi pi-fw pi-lock',
-            //                     routerLink: ['/auth/access'],
-            //                     data: { fullPage: true },
-            //                 },
-            //                 {
-            //                     label: 'Register',
-            //                     icon: 'pi pi-fw pi-user-plus',
-            //                     routerLink: ['/auth/register'],
-            //                     data: { fullPage: true },
-            //                 },
-            //                 {
-            //                     label: 'Forgot Password',
-            //                     icon: 'pi pi-fw pi-question',
-            //                     routerLink: ['/auth/forgotpassword'],
-            //                     data: { fullPage: true },
-            //                 },
-            //                 {
-            //                     label: 'New Password',
-            //                     icon: 'pi pi-fw pi-cog',
-            //                     routerLink: ['/auth/newpassword'],
-            //                     data: { fullPage: true },
-            //                 },
-            //                 {
-            //                     label: 'Verification',
-            //                     icon: 'pi pi-fw pi-envelope',
-            //                     routerLink: ['/auth/verification'],
-            //                     data: { fullPage: true },
-            //                 },
-            //                 {
-            //                     label: 'Lock Screen',
-            //                     icon: 'pi pi-fw pi-eye-slash',
-            //                     routerLink: ['/auth/lockscreen'],
-            //                     data: { fullPage: true },
-            //                 },
-            //             ],
-            //         },
-            //         {
-            //             label: 'Crud',
-            //             icon: 'pi pi-fw pi-pencil',
-            //             routerLink: ['/pages/crud'],
-            //         },
-            //         {
-            //             label: 'Timeline',
-            //             icon: 'pi pi-fw pi-calendar',
-            //             routerLink: ['/pages/timeline'],
-            //         },
-            //         {
-            //             label: 'Invoice',
-            //             icon: 'pi pi-fw pi-dollar',
-            //             routerLink: ['/pages/invoice'],
-            //         },
-            //         {
-            //             label: 'About Us',
-            //             icon: 'pi pi-fw pi-user',
-            //             routerLink: ['/pages/aboutus'],
-            //         },
-            //         {
-            //             label: 'Help',
-            //             icon: 'pi pi-fw pi-question-circle',
-            //             routerLink: ['/pages/help'],
-            //         },
-            //         {
-            //             label: 'Not Found',
-            //             icon: 'pi pi-fw pi-exclamation-circle',
-            //             routerLink: ['/pages/notfound'],
-            //         },
-            //         {
-            //             label: 'Empty',
-            //             icon: 'pi pi-fw pi-circle-off',
-            //             routerLink: ['/pages/empty'],
-            //         },
-            //         {
-            //             label: 'FAQ',
-            //             icon: 'pi pi-fw pi-question',
-            //             routerLink: ['/pages/faq'],
-            //         },
-            //         {
-            //             label: 'Contact Us',
-            //             icon: 'pi pi-fw pi-phone',
-            //             routerLink: ['/pages/contact'],
-            //         },
-            //     ],
-            // },
+            {
+                label: 'Pages',
+                icon: 'pi pi-fw pi-briefcase',
+                items: [
+                    {
+                        label: 'Landing',
+                        icon: 'pi pi-fw pi-globe',
+                        routerLink: ['/landing'],
+                        data: { fullPage: true },
+                    },
+                    {
+                        label: 'Auth',
+                        icon: 'pi pi-fw pi-user',
+                        items: [
+                            {
+                                label: 'Login',
+                                icon: 'pi pi-fw pi-sign-in',
+                                routerLink: ['/auth/login'],
+                                data: { fullPage: true },
+                            },
+                            {
+                                label: 'Error',
+                                icon: 'pi pi-fw pi-times-circle',
+                                routerLink: ['/auth/error'],
+                                data: { fullPage: true },
+                            },
+                            {
+                                label: 'Access Denied',
+                                icon: 'pi pi-fw pi-lock',
+                                routerLink: ['/auth/access'],
+                                data: { fullPage: true },
+                            },
+                            {
+                                label: 'Register',
+                                icon: 'pi pi-fw pi-user-plus',
+                                routerLink: ['/auth/register'],
+                                data: { fullPage: true },
+                            },
+                            {
+                                label: 'Forgot Password',
+                                icon: 'pi pi-fw pi-question',
+                                routerLink: ['/auth/forgotpassword'],
+                                data: { fullPage: true },
+                            },
+                            {
+                                label: 'New Password',
+                                icon: 'pi pi-fw pi-cog',
+                                routerLink: ['/auth/newpassword'],
+                                data: { fullPage: true },
+                            },
+                            {
+                                label: 'Verification',
+                                icon: 'pi pi-fw pi-envelope',
+                                routerLink: ['/auth/verification'],
+                                data: { fullPage: true },
+                            },
+                            {
+                                label: 'Lock Screen',
+                                icon: 'pi pi-fw pi-eye-slash',
+                                routerLink: ['/auth/lockscreen'],
+                                data: { fullPage: true },
+                            },
+                        ],
+                    },
+                    {
+                        label: 'Crud',
+                        icon: 'pi pi-fw pi-pencil',
+                        routerLink: ['/pages/crud'],
+                    },
+                    {
+                        label: 'Timeline',
+                        icon: 'pi pi-fw pi-calendar',
+                        routerLink: ['/pages/timeline'],
+                    },
+                    {
+                        label: 'Invoice',
+                        icon: 'pi pi-fw pi-dollar',
+                        routerLink: ['/pages/invoice'],
+                    },
+                    {
+                        label: 'About Us',
+                        icon: 'pi pi-fw pi-user',
+                        routerLink: ['/pages/aboutus'],
+                    },
+                    {
+                        label: 'Help',
+                        icon: 'pi pi-fw pi-question-circle',
+                        routerLink: ['/pages/help'],
+                    },
+                    {
+                        label: 'Not Found',
+                        icon: 'pi pi-fw pi-exclamation-circle',
+                        routerLink: ['/pages/notfound'],
+                    },
+                    {
+                        label: 'Empty',
+                        icon: 'pi pi-fw pi-circle-off',
+                        routerLink: ['/pages/empty'],
+                    },
+                    {
+                        label: 'FAQ',
+                        icon: 'pi pi-fw pi-question',
+                        routerLink: ['/pages/faq'],
+                    },
+                    {
+                        label: 'Contact Us',
+                        icon: 'pi pi-fw pi-phone',
+                        routerLink: ['/pages/contact'],
+                    },
+                ],
+            },
 
             // TODO
             // {
